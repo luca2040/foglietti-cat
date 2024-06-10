a1fadf34fa2225dc6b89d65a91dc6d79a59ba2cf
Luca <152313871+Luca2040@users.noreply.github.com>
2024-06-10 08:53:11 +0200

    better upload

--- CheshireCatAPI/API_upload.py
+++ CheshireCatAPI/API_upload.py
@@ -1,60 +1,62 @@
 import time
 import cheshire_cat_api as ccat
 import os
-import json
+import sys
 
 from websocket_functions import *
 from uploadfile import upload_file
 
+from config import config
+from log import log, delete_log
 
-config = ccat.Config(
-    base_url="localhost",
-    port=1865,
-    user_id="upload_websocket",
-    auth_key="",
-    secure_connection=False,
-)
-folder_path = "files/"
+if __name__ == "__main__":
+    config_filepath = sys.argv[1] if len(sys.argv) > 1 else "./config.json"
+    config.init(config_filepath)
 
-file_finished = False
+    delete_log()
 
+    cat_client = ccat.CatClient(
+        config=config,
+        on_open=on_open,
+        on_close=on_close,
+        on_message=on_message,
+        on_error=on_error,
+    )
 
-def on_message(message: str):
-    json_message = json.loads(message)
+    cat_client.connect_ws()
 
-    if json_message["type"] == "notification":
-        content_message: str = json_message["content"]
+    while not cat_client.is_ws_connected:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            log("KeyboardInterrupt", "ERROR")
+            cat_client.close()
 
-        print(content_message)
+            try:
+                sys.exit(1)
+            except SystemExit:
+                os._exit(1)
 
-        if content_message.startswith("Finished reading "):
-            global file_finished
-            file_finished = True
+    file_list = os.listdir(config.files_folder_path)
 
+    for file in file_list:
+        file_path = os.path.join(config.files_folder_path, file)
 
-cat_client = ccat.CatClient(
-    config=config,
-    on_open=on_open,
-    on_close=on_close,
-    on_message=on_message,
-    on_error=on_error,
-)
+        result = upload_file(file_path, file, config.rabbit_url, user=config.user_id)
+        log(result.json(), "INFO")
 
-cat_client.connect_ws()
+        while not config.get_message_state():
+            try:
+                time.sleep(0.5)
+            except KeyboardInterrupt:
+                log("KeyboardInterrupt", "ERROR")
+                cat_client.close()
 
-while not cat_client.is_ws_connected:
-    time.sleep(1)
+                try:
+                    sys.exit(1)
+                except SystemExit:
+                    os._exit(1)
 
-file_list = os.listdir(folder_path)
-for file in file_list:
-    file_path = os.path.join(folder_path, file)
+        config.reset_message_called()
 
-    result = upload_file(file_path, file, user=config.user_id)
-    print(result.json())
-
-    while not file_finished:
-        time.sleep(0.5)
-
-    file_finished = False
-
-cat_client.close()
+    cat_client.close()
